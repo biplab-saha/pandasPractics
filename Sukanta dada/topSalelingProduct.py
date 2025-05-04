470e8527c29e2ba710ca9a4edaeecfa999df685d
Biplab Saha <biplab5523x@gmil.com>
2025-05-05 00:27:12 +0530

    new file

--- Sukanta dada/topSalelingProduct.py
+++ Sukanta dada/topSalelingProduct.py
@@ -1,8 +1,9 @@
 import pandas as pd
-file=pd.read_csv('../sales_data.csv')
+file=pd.read_csv('sales_data.csv')
 df=pd.DataFrame(file)
 
 
 #top selling product
 topProduct=df.groupby('Product')['Quantity'].sum().sort_values(ascending=False).head(1)
 print(topProduct)
+
