b5754876d75da555ffea64e322ec91fb1b1696fd
Biplab Saha <biplab5523x@gmil.com>
2025-06-12 11:26:56 +0530

    new code add

--- Sukanta dada/topSalelingProduct.py
+++ Sukanta dada/topSalelingProduct.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
 file=pd.read_csv('sales_data.csv')
 df=pd.DataFrame(file)
 
 
 #top selling product
 topProduct=df.groupby('Product')['Quantity'].sum().sort_values(ascending=False).head(1)
-print(topProduct)
+# print(topProduct)
+print(df)
+plt.bar(df['Price'],df['Payment Method'])
+plt.show()
 
