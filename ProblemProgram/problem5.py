46c5c6d7feb6dd53ee33beacebbb01b698b45754
Biplab Saha <biplab5523x@gmil.com>
2025-07-05 10:48:20 +0530

    new project

--- ProblemProgram/problem5.py
+++ ProblemProgram/problem5.py
@@ -4,10 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 df=pd.read_csv('netflix_titles.csv')
 
-topCountry=df['country'].value_counts().head(10)
-print(topCountry)
-plt.figure(figsize=(10,6))
-plt.title('')
-plt.xlabel('')
-plt.ylabel('')
+top_con=df['country'].value_counts().head(10)
+top_con.plot(kind='bar',color='orange',edgecolor='black')
+plt.figure(figsize=(20,10))
+plt.title('top 10 countries')
+plt.xlabel('shows')
+plt.ylabel('countries')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
